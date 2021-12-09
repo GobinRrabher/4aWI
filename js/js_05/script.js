4908da738917aab1ed936a3eaa1fe318c313a1e8
GobinRRabher <robin.grabher@student.htldornbirn.at>
2021-12-09 08:42:46 +0100

    9.12

--- js/js_05/script.js
+++ js/js_05/script.js
@@ -1,6 +1,13 @@
 function generate(){
-    let num1 = parseInt(document.getElementById("num1").value);
-    let num2 = parseInt(document.getElementById("num2").value);
-    let random = Math.random() * (num2 - num1) + num1;
-    document.getElementById("output").innerHTML = random;
+    let length = parseInt(document.getElementById("length"));
+    let lowercase = document.getElementById("lowercase");
+
+    if(lowercase.checked){
+        lowercase = 1;
+    }
+    else{
+        lowercase = 0;
+    }
+
+    console.log(lowercase);
 }
