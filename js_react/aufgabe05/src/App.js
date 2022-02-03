d8a2a92562bdc445443ae82cefb0c9188b312781
GobinRRabher <robin.grabher@student.htldornbirn.at>
2022-02-03 09:46:57 +0100

    react 1. aufgabe 2/3

---
+++ js_react/aufgabe05/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import List from './components/List.js';
+import SuperButton from './components/SuperButton.js';
+
+function App() {
+  return (
+    <div className="App">
+      <List/>
+      <SuperButton/>
+    </div>
+  );
+}
+
+export default App;
